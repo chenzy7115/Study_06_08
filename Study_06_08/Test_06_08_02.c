eb59c610b2699069b6c02022dc60d35cd852d572
chenzy7115 <swczy@126.com>
2022-06-10 21:38:45 +0800

    struct

--- Study_06_08/Test_06_08_02.c
+++ Study_06_08/Test_06_08_02.c
@@ -3,12 +3,17 @@
 #include <stdio.h>
 #include <string.h>
 
+struct Book
+{
+	char name[20];
+	short price;
+};
+
 int main()
 {
-	printf("%d\n", (int)sizeof(char*));
-	printf("%d\n", (int)sizeof(int*));
-	printf("%d\n", (int)sizeof(short*));
-	printf("%d\n", (int)sizeof(float*));
-
+	struct Book b1 = { "C语言程序设计",55 };
+	printf("书名：%s\n", b1.name);
+	printf("价格：%d\n",b1.price);
+	return 0;
 }
 
