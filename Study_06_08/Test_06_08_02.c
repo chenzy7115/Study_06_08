bea21a54e90eb561f5fb91e18c919fb732fe5c2b
chenzy7115 <swczy@126.com>
2022-06-10 21:38:45 +0800

    strcpy的使用struct

--- Study_06_08/Test_06_08_02.c
+++ Study_06_08/Test_06_08_02.c
@@ -3,12 +3,30 @@
 #include <stdio.h>
 #include <string.h>
 
+struct Book
+{
+	char name[20];
+	short price;
+};
+
 int main()
 {
-	printf("%d\n", (int)sizeof(char*));
-	printf("%d\n", (int)sizeof(int*));
-	printf("%d\n", (int)sizeof(short*));
-	printf("%d\n", (int)sizeof(float*));
+	struct Book b1 = { "C语言程序设计",55 };
+	struct Book* pb = &b1;
 
+	printf("书名：%s\n",(*pb).name);
+	printf("价格：%d\n",(*pb).price);
+
+	printf("书名：%s\n", pb->name);
+	printf("价格：%d\n", pb->price);
+
+	pb->price = 15;
+	strcpy(b1.name, "C++程序设计"); //strcpy是string copy的意思
+	printf("修改过的书名：%s\n", pb->name);
+
+	printf("修改后的价格：%d\n", pb->price);
+
+	
+	return 0;
 }
 
